359e38d23b087aa3f5d316a738c09f9eb3d6cca0
hesuiyuan <1587374873@qq.com>
2017-02-24 00:10:55 +0800

    opop

--- china-mobile/js/intexl.js
+++ china-mobile/js/intexl.js
@@ -1,17 +1,36 @@
 window.onload=function(){
     var con=document.querySelector(".content");
-    var num=0;
-    setInterval(function () {
+    var num=1;
+    var t1=setInterval(move,2000);
+    function move() {
+        bottom=false;
         con.style.transition="all 0.5s";
         num++;
         con.style.marginLeft=-295*num+"px"
-    },2000);
+    }
     con.addEventListener("transitionend",function(){
-        if (num==12){
-            con.style.marginLeft="0";
+        if (num==13){
+             con.style.marginLeft="-295px";
             num=0;
             con.style.transition="none"
         }
+        bottom=true;
     });
+    var box=document.querySelector(".box");
+    box.onmousemove=function(){
+              clearInterval(t1)
+    };
+    box.onmouseout=function(){
+        t1=setInterval(move,2000)
+    };
+    var left=document.querySelector(".left");
+    var right=document.querySelector(".right");
+    var bottom=true;
+      right.onclick=function(){
+          if (bottom){
+          move();
+          }
+      };
+
 //    最后
 };
--- tianmao-new/js/indexl.js
+++ tianmao-new/js/indexl.js
@@ -3,6 +3,7 @@ window.onload=function(){
 
 var list=document.getElementsByClassName("list");
 var dian=document.getElementsByClassName("dian");
+    var content=document.querySelector(".content")
 for (let i=0;i<dian.length;i++){
     dian[i].onmouseover=function(){
         for (let j=0;j<list.length;j++){
@@ -28,17 +29,56 @@ function move(){
     }
     list[num].style.opacity="1";
     dian[num].style.background="#F1F1F1"
+    // content.onmouseover(function(){
+    //     clearInterval(t1)
+    // })
 }
+    // content.onmouseout(function(){
+    //     t1=setInterval(move,2000)
+    // })
+
+
 //         02
 var content=document.getElementsByClassName("box")[0];
 content.onmouseover=function(){
     clearInterval(t1);
-}
+};
 content.onmouseout=function(){
         t1=setInterval(move,2000);
+    };
+    var sum=document.querySelector(".box-sum");
+    var an=document.querySelectorAll(".lists");
+    var tops=document.querySelector(".top");
+    window.onscroll=function(){
+        var obj=document.body.scrollTop==0?document.documentElement:document.body;
+        if (obj.scrollTop>1500){
+            animate(sum,{width:40,height:360},1)
+            tops.style.height="50px"
+        }else {
+            animate(sum,{width:0,height:0},1)
+            tops.style.height="0"
+        }
+    };
+//    22222
+    var an=document.querySelectorAll(".lists");
+    var chuxinpin=document.querySelectorAll(".chuxinpin");
+    var sections=document.querySelector(".sectionss");
+    var fooers=document.querySelector(".fooer")
+    for(let i=0;i<an.length;i++){
+        var obj=document.body.scrollTop==0?document.documentElement:document.body;
+        an[i].onclick=function(){
+            if (i==6){
+                 var sect=sections.offsetTop;
+                animate(obj,{scrollTop:sect-120},50)
+            }else {
+                if (i==7){
+                  var fooe=fooers.offsetTop;
+                    animate(obj,{scrollTop:fooe},50)
+                }
+            }
+            var s=chuxinpin[i].offsetTop;
+                animate(obj,{scrollTop:s-110},50)
+        }
     }
-    
-    
-    
 //最后
 };
